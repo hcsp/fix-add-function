3d58dbe539b54c8a30c9794e6da5068eda654b62
Bo Zhang <bo@gradle.com>
2019-04-17 10:50:24 +0800

    Test PR (#3)
    Test PR (#3)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,8 @@
 public class Solution {
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
+
 
     public static void main(String[] args) {
         System.out.println("1+1=" + add(1, 1));
