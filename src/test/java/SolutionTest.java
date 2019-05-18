adb16aba74c1b9a49b37204832f281b466752559
Bo Zhang <bo@gradle.com>
2019-05-18 16:10:45 +0800

    Auto format Java code

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,13 +1,13 @@
+import static org.junit.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class SolutionTest {
+class SolutionTest {
     @Test
-    void addTest() {
-        org.junit.jupiter.api.Assertions.assertEquals(Solution.add(1, 1), 2);
-org.junit.jupiter.api.Assertions.assertEquals(Solution.add(1, 2), 3);
-org.junit.jupiter.api.Assertions.assertEquals(Solution.add(-1, 1), 0);
-org.junit.jupiter.api.Assertions.assertEquals(Solution.add(Integer.MAX_VALUE, Integer.MIN_VALUE), -1);
-
+    public void addTest() {
+        assertEquals(Solution.add(1, 1), 2);
+        assertEquals(Solution.add(1, 2), 3);
+        assertEquals(Solution.add(-1, 1), 0);
+        assertEquals(Solution.add(Integer.MAX_VALUE, Integer.MIN_VALUE), -1);
     }
 }
