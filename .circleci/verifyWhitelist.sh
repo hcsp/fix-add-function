a0580766fa344ba3df3c90b8133674ce05aeff39
Bo Zhang <bo@gradle.com>
2019-07-03 23:46:20 +0800

    Refine verifyWhitelist.sh

--- .circleci/verifyWhitelist.sh
+++ .circleci/verifyWhitelist.sh
@@ -39,8 +39,8 @@ do
 done
 
 if [ ${#RESULT[@]} -ne 0 ]; then
-    echo "We only allow the modification of these files:\n${WHITELIST[@]}\n\nThe files you modified:\n${RESULT[@]}"
-    echo "\n--------------------\n"
-    echo "我们只允许你修改以下文件:\n${WHITELIST[@]}\n\n但是你修改了:\n${RESULT[@]}"
+    echo -e "We only allow the modification of these files:\n${WHITELIST[@]}\n\nThe files you modified:\n${RESULT[@]}"
+    echo -e "\n--------------------\n"
+    echo -e "我们只允许你修改以下文件:\n${WHITELIST[@]}\n\n但是你修改了:\n${RESULT[@]}\n"
     exit 1
 fi
