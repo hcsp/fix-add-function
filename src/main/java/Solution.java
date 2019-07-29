5bfdf3439b4b449fa875df48701027f0cdcf93c5
ambition-to <45473761+ambition-to@users.noreply.github.com>
2019-07-29 14:10:01 +0800

    任务 3 提交 (#112)
    任务 3 提交 (#112)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,5 @@
 public class Solution {
-    public static int add(int a, int b) {
-        return a - b;
-    }
+    public static int add(int a, int b) { return a + b; }
 
     public static void main(String[] args) {
         System.out.println("1+1=" + add(1, 1));
