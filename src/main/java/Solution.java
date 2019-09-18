3570bd7529d511090438cdbd0e847ca3bc7fd4cc
aalukaiqi <55475086+aalukaiqi@users.noreply.github.com>
2019-09-18 13:27:37 +0800

    你加号写成减号了. (#180)
    你加号写成减号了. (#180)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public static int add(int a, int b) {
-        return a - b;
+        //hhh
+        return a + b;
     }
 
     public static void main(String[] args) {
