edf15a7b570bf961088c02fe3cf7b722e45e527d
e <985686843@QQ.COM>
2019-09-28 16:53:00 +0800

    add

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,6 +4,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println("1+1=" + add(1, 1));
+        System.out.println("1+1=" + add(1, 11));
     }
 }
