74cc9e52d0a92313a1c78e8090490692105862c5
zn1850018040 <1061973972@qq.com>
2019-12-15 22:01:23 +0800

    之前一直提交不上，在pushing的过程中，一直在登录github，删除项目重新clone，尝试

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,8 @@
+package main.java;
+
 public class Solution {
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public static void main(String[] args) {
