168347f5fd8d8a1f6bdcef2ed9f90b1496776e2d
ergouwang <mrergouwang@gmail.com>
2020-05-09 20:35:33 +0800

    我发现你这里把代码写错了，应该是+号，不应该是-号。

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,8 @@
+import sun.lwawt.macosx.CSystemTray;
+
 public class Solution {
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public static void main(String[] args) {
