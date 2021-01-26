ce482905fcb51cc2d2e92785b3a0e3d0b2fdbd80
joi-chen <78025980+joi-chen@users.noreply.github.com>
2021-01-26 23:10:27 +0800

    我把减号改成加号了 (#495)
    我把减号改成加号了 (#495)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,11 @@
 public class Solution {
-    public static int add(int a, int b) {
-        return a - b;
+    public static int add(int a, int b)
+    {
+        return a + b;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("1+1=" + add(1, 1));
     }
 }
